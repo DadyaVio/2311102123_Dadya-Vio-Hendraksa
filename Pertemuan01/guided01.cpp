347d34de59bcbbb421eab5813031c5e8ebb9a86c
DadyaVio <dadyavio@gmail.com>
2024-03-12 11:40:56 +0700

    Modul1

--- Pertemuan01/guided01.cpp
+++ Pertemuan01/guided01.cpp
@@ -7,24 +7,30 @@ int main () {
     char op;
     float num1, num2;
 
+    // It allows user to enter operator i.e. +, -, *, /
     cout << "Enter operator (+,-,*,/)";
     cout << endl;
     cin >> op;
 
+    // It allows user to enter the operands
     cout << "Enter two operands : ";
     cin >> num1 >> num2;
 
-    switch (op)
-    {
+    // Switch statement begins
+    switch (op) {
+    // If user enters +
     case '+':
         cout << "Result: " << num1 + num2;
         break;
+    // If user enters -
     case '-':
         cout << "Result: " << num1 - num2;
         break;
+    // If user enters *
     case '*':
         cout << "Result: " << num1 * num2;
         break;
+    // If user enters /
     case '/':
         if (num2 != 0) {
             cout << "Result: " << fixed << setprecision(2) << num1 / num2;
@@ -32,8 +38,11 @@ int main () {
             cout << "Error! Division by zero is not allowed.";
         }
         break;
+    // If the operator is other than +, -, * or /,
+    // errror message will display
     default:
         cout << "Error! Operator is not correct";
-    }
+    } // switch statement ends
+    
     return 0;
 }
